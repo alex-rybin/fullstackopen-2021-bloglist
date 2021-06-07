ad6d6d92ea1c0f83952f8c768d43e16ffa6a7b4e
Александр Рыбин <alex_rybin@playrix.com>
2021-06-07 23:25:42 +0300

    Add exercise 4.10

--- tests/blogsApi.test.js
+++ tests/blogsApi.test.js
@@ -45,6 +45,28 @@ test('blogs have id property', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('blogs added when sent by post request', async () => {
+    const newBlog = {
+        title: 'We Are The People',
+        author: 'Empire of the Sun',
+        url: 'https://music.apple.com/ru/album/we-are-the-people/712862605?i=712862710&l=en',
+        likes: 998,
+    }
+
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(response.body[2].title).toBe('We Are The People')
+    expect(response.body[2].author).toBe('Empire of the Sun')
+    expect(response.body[2].url).toBe('https://music.apple.com/ru/album/we-are-the-people/712862605?i=712862710&l=en')
+    expect(response.body[2].likes).toBe(998)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
